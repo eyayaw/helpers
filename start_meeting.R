3064301c3ed3a30811f1b02d5da5962c179a15da
Eyayaw Beze <49317723+eyayaw@users.noreply.github.com>
2022-11-20 15:53:10 +0100

    Delete start_meeting.R

--- start_meeting.R
+++
@@ -1,9 +0,0 @@
-start_meeting <- function(course.name) {
-  if (!is.character(course.name)) {
-    course.name <- eval(substitute(substitute(course.name)), parent.frame())
-    if (is.symbol(course.name)) course.name <- as.character(course.name)
-  }
-  meeting <- read.table("meeting.txt", header = TRUE)
-  message("Use password: ", meeting[which(meeting$course_name == course.name), "password"])
-  browseURL(meeting[which(meeting$course_name == course.name), "url"])
-}
