f774fa2fb86c6503c48f287a3aa3eb880c032cab
Eyayaw Beze <49317723+eyayaw@users.noreply.github.com>
2022-11-20 13:58:07 +0100

    Update rmd_boilerplate.R

--- rmd_boilerplate.R
+++ rmd_boilerplate.R
@@ -3,10 +3,8 @@ rmd_boilerplate <- function(input,
                             date = Sys.Date(),
                             author = "Eyayaw Beze",
                             output = "pdf_document",
-                            more_text = "") {
-  if (!grepl("[.][Rr]?md$", input)) {
-    input <- paste0(input, ".Rmd")
-  }
+                            more_metadata = "geometry:\n  - left=3cm\n  - right=3cm\n  - top=3cm\n  - bottom=3cm", more_text="") {
+
   date <- sprintf(r"{"`r format.Date('%s', '%%B %%d, %%Y')`"}", date)
   metadata <- paste0(
     "---\n",
@@ -14,20 +12,33 @@ rmd_boilerplate <- function(input,
     "author: ", author, "\n",
     "date: ", date, "\n",
     "output: ", output, "\n",
-    "---", "\n",
-    "\n\n",
+    "colorlinks: ", "yes", "\n",
+    "fontsize: ", "12pt", "\n",
+    paste(more_metadata, collapse = "\n"),
+    "\n",
+    "---", "\n\n\n",
     more_text,"\n\n",
     "```{r setup, include=FALSE}
-    knitr::opts_chunk$set(echo = TRUE)
-    ```"
+knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
+```"
   )
-  if (!file.exists(input)) {
-    message("The metadata has been written to `", input, "` successfully.")
-    writeLines(metadata, input)
+
+  dir = NULL
+  try(dir <- dirname(input))
+  if (dir.exists(dir)) {
+    if (!grepl("[.][Rr]?md$", input)) {
+      input <- paste0(input, ".Rmd") # append a file ext if not provided
+    }
+    if (!file.exists(input)) {
+      message("The metadata has been written to `", input, "` successfully.")
+      writeLines(metadata, input)
+      rstudioapi::navigateToFile(input)
+    } else {
+      warning(sprintf("`%s` already exists.\nGive another filename.", input),
+              call. = FALSE)
+    }
   } else {
-    stop(
-      sprintf("'%s` already exists.\nGive another file name.", input),
-      call. = FALSE)
+    warning('The directory `', dir, "` does not seem to exist. File hasn't been written", call. = FALSE)
+    cat(metadata, sep = "\n")
   }
-  rstudioapi::navigateToFile(input)
 }
